cd2040bebf710bd9f45ab60e22cc0af446032cbc
jigs-1 <jignashgeda@gmail.com>
2026-01-07 12:46:34 +0530

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,12 +1,14 @@
 from ngram_model import build_ngram_model, generate_text
 
+
 def main():
     with open("data/conan_doyle.txt", "r", encoding="utf-8") as f:
         text = f.read()
 
     model = build_ngram_model(text, n=5)
-    generated = generate_text(model, num_words=100)
+    generated = generate_text(model, n=5, length=100)
     print(generated)
+
 
 if __name__ == "__main__":
     main()
