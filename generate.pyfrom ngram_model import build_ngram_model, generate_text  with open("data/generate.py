3d63426db5507f08e507a9d2a7a863ca07b55fd3
jigs-1 <jignashgeda@gmail.com>
2026-01-07 12:22:23 +0530

    Update generate.py

--- generate.pyfrom ngram_model import build_ngram_model, generate_text  with open("data/generate.py
+++ generate.pyfrom ngram_model import build_ngram_model, generate_text  with open("data/generate.py
@@ -1,17 +1,13 @@
 from ngram_model import build_ngram_model, generate_text
 
-with open("data/conan_doyle.txt", "r", encoding="utf-8") as f:
-    text = f.read()
+def main():
+    with open("data/conan_doyle.txt", "r", encoding="utf-8") as f:
+        text = f.read()
 
-model = build_ngram_model(text, n=5)
+    model = build_ngram_model(text, n=5)
+    generated = generate_text(model, num_words=100)
+    print(generated)
 
-samples = [
-    "the day was very",
-    "mr sherlock holmes",
-    "i had my doubts"
-]
 
-for s in samples:
-    print("Input:", s)
-    print("Output:", generate_text(s, model))
-    print()
+if __name__ == "__main__":
+    main()
